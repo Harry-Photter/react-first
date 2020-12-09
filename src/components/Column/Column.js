88f44f639c866d331009f33c34decd20040449f8
Mateusz Furman <mateuszfurman.nf@gmail.com>
2020-12-09 16:05:43 +0100

    Add Cart and Icon

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import Creator from '../Creator/Creator.js';
+import { settings } from '../../data/dataStore';
+import Icon from '../Icon/Icon.js';
 
-const Column = props => (
-  <section className={styles.component}>
-    <h3 className={styles.title}>{props.title}</h3>
-  </section>
-);
 
-Column.propTypes = {
-  title: PropTypes.node.isRequired,
+class Column extends React.Component {
+  state = {
+    cards: this.props.cards || [],
+  }
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+   }
+
+  addCard(title) {
+    this.setState(state => (
+      {
+        cards: [
+          ...state.cards,
+          {
+            key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
+            title,
+          }
+        ]
+      }
+    ));
+  }
+
+  render() {
+    return (
+      <section className={styles.component}>
+        <h3 className={styles.title}>
+          <span className={styles.icon}>
+            <Icon name={this.props.icon}/>
+          </span>
+          {this.props.title}
+        </h3>
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Column;
