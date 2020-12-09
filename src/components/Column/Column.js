5c2810a8afbc50704ff7c220e71ab22d63cfe429
Mateusz Furman <mateuszfurman.nf@gmail.com>
2020-12-09 23:23:57 +0100

    Fix addCard

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Creator from '../Creator/Creator.js';
+import Card from '../Card/Card.js'
 import { settings } from '../../data/dataStore';
 import Icon from '../Icon/Icon.js';
 
@@ -37,6 +38,11 @@ class Column extends React.Component {
           </span>
           {this.props.title}
         </h3>
+        <div className={styles.cards}>
+          {this.state.cards.map(({ key, ...cardProps }) => (
+            <Card key={key} {...cardProps} />
+          ))}
+        </div>
         <div className={styles.creator}>
           <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
         </div>
